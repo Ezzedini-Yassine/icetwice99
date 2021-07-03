24b82c021430b009d077d82402a8524ab268ff61
Yassine-Ezzedini <yassine.ezzedini@esprit.tn>
2021-07-03 23:23:49 +0100

    94 - reorganized the code, instead of displaying products in admin dashboard i moved it to products

--- client/src/components/cards/AdminProductCard.js
+++ client/src/components/cards/AdminProductCard.js
@@ -14,7 +14,7 @@ const AdminProductCard = ({ product }) => {
           alt=""
           src={images && images.length ? images[0].url : ""}
           style={{ heigth: "150px", objectFit: "cover" }}
-          className="m-2"
+          className="p-1"
         />
       }
     >
