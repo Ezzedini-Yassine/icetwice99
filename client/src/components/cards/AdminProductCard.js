102c201a4bf662ac5296aaa2c1d63e9f8cad517f
Yassine-Ezzedini <yassine.ezzedini@esprit.tn>
2021-07-04 00:57:35 +0100

    95 - added default image for product + edit and delete buttons to all products

--- client/src/components/cards/AdminProductCard.js
+++ client/src/components/cards/AdminProductCard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Card } from "antd";
+import imagenotfound from "../../images/imagenotfound.jpg";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
 const { Meta } = Card;
 
@@ -9,16 +11,24 @@ const AdminProductCard = ({ product }) => {
 
   return (
     <Card
+      hoverable
       cover={
         <img
           alt=""
-          src={images && images.length ? images[0].url : ""}
+          src={images && images.length ? images[0].url : imagenotfound}
           style={{ heigth: "150px", objectFit: "cover" }}
           className="p-1"
         />
       }
+      actions={[
+        <EditOutlined className="text-warning" />,
+        <DeleteOutlined className="text-danger" />,
+      ]}
     >
-      <Meta title={title} description={description} />
+      <Meta
+        title={title}
+        description={`${description && description.substring(0, 40)} ...`}
+      />
     </Card>
   );
 };
