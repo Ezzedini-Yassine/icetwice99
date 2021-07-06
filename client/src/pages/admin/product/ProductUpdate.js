8c7733f3c18475a5cc8c6f50cc27d0753724879c
Yassine-Ezzedini <yassine.ezzedini@esprit.tn>
2021-07-06 23:59:11 +0100

    99 - created route and controller to find product by his slug in backend / found a way to select slug from the frontend, puisque my whole add is surrounded by <BrouserRouter> fel index.js, i can access the match object in the props in ProductUpdate.js

--- client/src/pages/admin/product/ProductUpdate.js
+++ client/src/pages/admin/product/ProductUpdate.js
@@ -8,9 +8,10 @@ import { getCategories, getCategorySubs } from "../../../functions/category";
 import FileUpload from "../../../components/forms/FileUpload";
 import { LoadingOutlined } from "@ant-design/icons";
 
-const ProductUpdate = () => {
+const ProductUpdate = ({ match }) => {
   const { user } = useSelector((state) => ({ ...state }));
-
+  //router
+  const { slug } = match.params;
   return (
     <div className="container-fluid">
       <div className="row">
@@ -20,6 +21,7 @@ const ProductUpdate = () => {
 
         <div className="col-md-10">
           <h4>Product Update</h4>
+          {JSON.stringify(slug)}
           <hr />
         </div>
       </div>
