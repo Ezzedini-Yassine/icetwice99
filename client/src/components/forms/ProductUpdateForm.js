695f7b047635f55b7fc8abcb0e6d967024927faa
Yassine-Ezzedini <yassine.ezzedini@esprit.tn>
2021-07-08 02:48:02 +0100

    104 - display the list of subcategories and the ones related to that exact product

--- client/src/components/forms/ProductUpdateForm.js
+++ client/src/components/forms/ProductUpdateForm.js
@@ -11,6 +11,8 @@ const ProductUpdateForm = ({
   handleCategoryChange,
   categories,
   subOptions,
+  arrayOfSubs,
+  setArrayOfSubs,
 }) => {
   //destructure
   const {
@@ -199,6 +201,25 @@ const ProductUpdateForm = ({
             ))}
         </select>
       </div>
+
+      <div>
+        <label>Sub Categories</label>
+        <Select
+          mode="multiple"
+          style={{ width: "100%" }}
+          placeholder="Please select"
+          value={arrayOfSubs}
+          onChange={(value) => setArrayOfSubs(value)}
+        >
+          {subOptions.length &&
+            subOptions.map((s) => (
+              <Option key={s._id} value={s._id}>
+                {s.name}
+              </Option>
+            ))}
+        </Select>
+      </div>
+
       <br />
       <button className="btn btn-outline-info">Save</button>
     </form>
